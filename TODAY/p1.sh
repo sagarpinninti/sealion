9862d484a6256ad0af7a36b23729cad1b33f8a5d
sagar Pinninti <31984052+sagarpinninti@users.noreply.github.com>
2023-04-14 01:13:48 -0700

    Delete TODAY directory

--- TODAY/p1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for img in $* ; do
-curl -sq https://registry.hub.docker.com/v1/repositories/$img/tags |jq '.[]|.name'|grep -v '-'|grep [0-9]|grep -F '.'|sort -Vr|tr -d '[:alpha:]|"|-'|
-grep -Ev "master|beta|latest|onbuild|test|unstable|light|full" |
-awk '!NF || !seen[$0]++'|awk 'NF > 0'|head -2|sed -e "s#^#${img}:#"
-done
